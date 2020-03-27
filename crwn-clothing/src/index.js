cf72a8488db5dcdfaf7261a9620e3024c4127222
Jesse <jessew@CRS-KWOO-MAC.attlocal.net>
2020-03-27 10:24:40 -0700

    routing

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* BrowserRoute wrap around our App component, gives our App all routing functionality */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
