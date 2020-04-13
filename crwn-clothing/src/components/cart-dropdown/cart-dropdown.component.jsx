62c8a753e1152089edefb5823e3da2e1d34fc240
Jesse <jessew@CRS-KWOO-MAC.attlocal.net>
2020-04-13 16:31:08 -0700

    start checkout page

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
@@ -9,14 +10,20 @@ import { selectCartItems } from '../../redux/cart/cart.selector';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
-      {cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className="empty-message">Your cart is empty</span>
+      )}
     </div>
-    <CustomButton>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick={() => history.push('/checkout')}>
+      GO TO CHECKOUT
+    </CustomButton>
   </div>
 );
 
@@ -26,4 +33,5 @@ const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+// With HOC, it's evaluating inside out
+export default withRouter(connect(mapStateToProps)(CartDropdown));
---
+++ crwn-clothing/src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './checkout.styles.scss';
+
+const CheckoutPage = () => <div className="checkoutpage">CHECKOUT PAGE</div>;
+
+export default CheckoutPage;
