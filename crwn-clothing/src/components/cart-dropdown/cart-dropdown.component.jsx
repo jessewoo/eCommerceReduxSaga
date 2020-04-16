1b08111a4589191126fa3e457038535f6114bd6c
Jesse <jessew@CRS-KWOO-MAC.attlocal.net>
2020-04-16 16:50:43 -0700

    checkout page

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,25 +7,36 @@ import CartItem from '../cart-item/cart-item.component';
 
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems } from '../../redux/cart/cart.selector';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      {cartItems.length ? (
-        cartItems.map((cartItem) => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))
-      ) : (
-        <span className="empty-message">Your cart is empty</span>
-      )}
+// otherProps - have access to dispatch, history
+const CartDropdown = ({ cartItems, history, /*...otherProps*/ dispatch }) => {
+  // Have access to dispatch, location, match
+  // console.log(otherProps);
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
+      </div>
+      <CustomButton
+        onClick={() => {
+          history.push('/checkout');
+          dispatch(toggleCartHidden());
+        }}
+      >
+        GO TO CHECKOUT
+      </CustomButton>
     </div>
-    <CustomButton onClick={() => history.push('/checkout')}>
-      GO TO CHECKOUT
-    </CustomButton>
-  </div>
-);
+  );
+};
 
 // Never gets re-rendered if there is nothing related to that gets udpated
 // Use selector cart
@@ -34,4 +45,6 @@ const mapStateToProps = createStructuredSelector({
 });
 
 // With HOC, it's evaluating inside out
+// Connect passes in dispatch as a prop into our component if we don't supply a 2nd argument to connect
+// Because if we need to write one off dispatches, no reason to write mapDispatchToProps
 export default withRouter(connect(mapStateToProps)(CartDropdown));
